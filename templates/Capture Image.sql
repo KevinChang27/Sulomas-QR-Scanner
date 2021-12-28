fdc0d1f9279e20b75383c4baf89bf8c827aae4ff
KevinChang27 <92295558+KevinChang27@users.noreply.github.com>
2021-12-28 16:17:16 +0800

    Delete templates directory

--- templates/Capture Image.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `images` (
-  `img_name` varchar(255) NOT NULL,
-  `img_data` longblob NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
- 
-ALTER TABLE `images` ADD UNIQUE KEY `img_name` (`img_name`);
